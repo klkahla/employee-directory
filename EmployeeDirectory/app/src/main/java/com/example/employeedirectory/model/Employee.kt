c39c3968d7d29acb53c0fd653efa2caebb86c07d
Katy Kahla <katykahla@gmail.com>
2024-08-10 15:18:26 -0600

    employee data class
    add data class

--- EmployeeDirectory/app/src/main/java/com/example/employeedirectory/model/Employee.kt
+++ EmployeeDirectory/app/src/main/java/com/example/employeedirectory/model/Employee.kt
@@ -1,4 +1,30 @@
 package com.example.employeedirectory.model
 
-class Employee {
-}
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EmployeeList(
+    val items: List<Employee> = listOf()
+)
+
+@Serializable
+data class Employee (
+    /*
+    {
+      "uuid": "0d8fcc12-4d0c-425c-8355-390b312b909c",
+      "full_name": "Justine Mason",
+      "phone_number": "5553280123",
+      "email_address": "jmason.demo@squareup.com",
+      "biography": "Engineer on the Point of Sale team.",
+      "photo_url_small": "https://s3.amazonaws.com/sq-mobile-interview/photos/16c00560-6dd3-4af4-97a6-d4754e7f2394/small.jpg",
+      "photo_url_large": "https://s3.amazonaws.com/sq-mobile-interview/photos/16c00560-6dd3-4af4-97a6-d4754e7f2394/large.jpg",
+      "team": "Point of Sale",
+      "employee_type": "FULL_TIME"
+    }
+     */
+    val uuid: String = "",
+    val fullName: String = "",
+    val phoneNumber: String = "",
+    val email: String = "",
+    val bio: String = "",
+)
